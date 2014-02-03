b7cf315fb0df7b3a4691623717de9007667c8a9b
da-vid <dave.wong@gmail.com>
2014-02-02 20:38:07 -0800

    cleaned up

--- js/quicklist.js
+++ js/quicklist.js
@@ -11,38 +11,12 @@ angular.module("quicklist", ["firebase"])
 
             //form validation pattern: http://stackoverflow.com/a/18747273
             $scope.moreThanWhitespace = /\S/;
-            // $scope.loaded = false;
-
 
             $scope.addItem = function() {
                 if ($scope.addItemForm.addItemBox.$valid) {
                     $scope.items.$add({ID: $scope.nextItemID(), name: $scope.itemName, checked: false});
                 }
                 $scope.itemName = "";
-            };
-
-            $scope.nextItemID = function() {
-                var maxItemID = 0;
-                var keys = $scope.items.$getIndex();
-
-                //https://www.firebase.com/docs/angular/reference.html#getindex
-                keys.forEach(function (key, i) {
-                    if($scope.items[key].ID > maxItemID) {
-                        maxItemID = $scope.items[key].ID;
-                    }
-                });
-                return maxItemID + 1;
-            };
-
-            $scope.itemCount = function() {
-                var count = 0;
-                var keys = $scope.items.$getIndex();
-
-                //https://www.firebase.com/docs/angular/reference.html#getindex
-                keys.forEach(function (key, i) {
-                    count++;
-                });
-                return count;
             };
 
             $scope.deleteItem = function(itemID) {
@@ -62,6 +36,29 @@ angular.module("quicklist", ["firebase"])
             $scope.items.$on("loaded", function() {
                 $scope.loaded = true;
             });
+
+            $scope.nextItemID = function() {
+                var maxItemID = 0;
+                var keys = $scope.items.$getIndex();
+
+                //https://www.firebase.com/docs/angular/reference.html#getindex
+                keys.forEach(function (key, i) {
+                    if($scope.items[key].ID > maxItemID) {
+                        maxItemID = $scope.items[key].ID;
+                    }
+                });
+                return maxItemID + 1;
+            };
+
+            $scope.itemCount = function() {
+                var count = 0;
+                var keys = $scope.items.$getIndex();
+
+                keys.forEach(function (key, i) {
+                    count++;
+                });
+                return count;
+            };
         }
     ]);
 
